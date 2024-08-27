851472fca5224e33dd322bff530c55911210ee5b
montvlein <fabricio@setandforget.io>
2024-08-27 16:39:02 -0300

    .

--- src/utils/sendFormData.js
+++ src/utils/sendFormData.js
@@ -1,5 +1,5 @@
 export default async function sendFormData(formData) {
-    const response = await fetch('https://4575-34-133-99-197.ngrok-free.app/responses/create', {
+    const response = await fetch('https://happy-mixed-gaura.glitch.me/https://4575-34-133-99-197.ngrok-free.app/responses/create', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
